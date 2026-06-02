ada3560b08b464a804565a95603bbfd577614a53
Ilias Anwari <ilias.anwari@gmail.com>
2026-06-02 10:41:34 +0200

    Using the ollama library. Setting up a defined output using functions: Chat and

---
+++ start-2.py
@@ -0,0 +1,20 @@
+#install the right dependencies
+#using python ollama library
+
+import ollama
+
+
+#print("Hallo Was geht ab")
+response = ollama.list()
+#print(response)
+
+res = ollama.chat(
+    model= "llama3.2:3b",
+    messages= [
+        {"role": "system" , "content": "You are a helpful career coach"},
+        {"role": "user" , "content": "Give me 3 best practises of how to look for jobs" }
+
+    ]
+)
+print(res.message.content)
+
